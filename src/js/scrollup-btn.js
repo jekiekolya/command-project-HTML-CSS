2bf294d0498785152ef98c77dcf088da613eee05
str1kus <thealexx@gmail.com>
2022-08-27 18:14:00 +0300

    slider, scroll-up, some bugs correction

---
+++ src/js/scrollup-btn.js
@@ -0,0 +1,19 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
